71bc19d1966c5f462825a993e5f0a2182d18847c
gjbex <geertjan.bex@uhasselt.be>
2016-09-02 13:54:18 +0200

    Batch system environment variables are now retrieved from config file

--- scripts/alog.py
+++ scripts/alog.py
@@ -8,16 +8,17 @@ import socket
 import sys
 import time
 
+from vsc.atools.config import get_var_config, ConfigFileError
 from vsc.atools.log_parser import LogEvent
 from vsc.atools.utils import EnvVarError
 
 
-def get_log_name():
+def get_log_name(var_names):
     '''Compute the log name from the environment variables defined
-    by PBS'''
+    by the batch system'''
     try:
-        job_name = os.environ['PBS_JOBNAME']
-        job_id = os.environ['PBS_JOBID']
+        job_name = os.environ[var_names['job_name_var']]
+        job_id = os.environ[var_names['job_id_var']]
     except KeyError as error:
         raise EnvVarError(error.args[0])
     job_name = re.sub(r'-\d+$', '', job_name)
@@ -25,10 +26,10 @@ def get_log_name():
     return '{0}.log{1}'.format(job_name, job_id)
 
 
-def create_start_msg():
+def create_start_msg(var_names):
     '''Create a work item start log entry'''
     try:
-        work_item_id = os.environ['PBS_ARRAYID']
+        work_item_id = os.environ[var_names['array_idx_var']]
     except KeyError as error:
         raise EnvVarError(error.args[0])
     hostname = socket.gethostname()
@@ -37,10 +38,10 @@ def create_start_msg():
     return str(event)
 
 
-def create_end_msg(exit_code):
+def create_end_msg(exit_code, var_names):
     '''Create a work item start log entry'''
     try:
-        work_item_id = os.environ['PBS_ARRAYID']
+        work_item_id = os.environ[var_names['array_idx_var']]
     except KeyError as error:
         raise EnvVarError(error.args[0])
     hostname = socket.gethostname()
@@ -60,13 +61,20 @@ if __name__ == '__main__':
                                                 'start or end')
     arg_parser.add_argument('--exit', type=int,
                             help='exit code of the work item')
+    arg_parser.add_argument('--conf', help='configuration file')
     options = arg_parser.parse_args()
+    if options.conf:
+        conf_filename = options.conf
+    else:
+        conf_filename = os.path.join(os.path.dirname(__file__),
+                                     '..', 'conf', 'atools.conf')
     try:
+        var_names = get_var_config(conf_filename)
         if options.state == 'start':
-            msg = create_start_msg()
+            msg = create_start_msg(var_names)
         else:
-            msg = create_end_msg(options.exit)
-        log_name = get_log_name()
+            msg = create_end_msg(options.exit, var_names)
+        log_name = get_log_name(var_names)
         log_file = posixfile.open(log_name, 'a')
         log_file.lock('w|')
         log_file.write(msg + os.linesep)
@@ -74,4 +82,12 @@ if __name__ == '__main__':
         log_file.close()
     except EnvVarError as error:
         sys.stderr.write('### error: {0}\n'.format(str(error)))
+        sys.exit(error.errno)
+    except IOError as error:
+        msg = '### IOError: {0}'.format(str(error))
+        sys.stderr.write(msg)
+        sys.exit(error.errno)
+    except ConfigFileError as error:
+        msg = '### IOError: {0}'.format(str(error))
+        sys.stderr.write(msg)
         sys.exit(error.errno)
