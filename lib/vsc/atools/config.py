0523ebebef9be944c6ac9f50899350384a434a4f
Geert Jan Bex <geertjan.bex@uhasselt.be>
2016-09-06 12:27:41 +0200

    First version of areduce

--- lib/vsc/atools/config.py
+++ lib/vsc/atools/config.py
@@ -17,16 +17,31 @@ class ConfigFileError(ArrayToolsError):
         return 'config file error: {0}'.format(self._message)
 
 
-def get_var_config(config_file_name):
+def get_var_config(config_filename):
     '''returns a dictionary with the relevant shell variable names for
     for the selected batch system'''
     config_parser = ConfigParser.SafeConfigParser()
     try:
-        config_parser.read(config_file_name)
+        config_parser.read(config_filename)
         batch_system = config_parser.get('global', 'batch_system', None)
         var_names = dict()
         for key, value in config_parser.items(batch_system):
             var_names[key] = value
         return var_names
+    except ConfigParser.Error as error:
+        raise ConfigFileError(str(error))
+
+
+def get_mode_config(config_filename, mode=None):
+    '''Retrieve the empty and reduce script for the given mode, or for
+    the default if none is given'''
+    config_parser = ConfigParser.SafeConfigParser()
+    try:
+        config_parser.read(config_filename)
+        if not mode:
+            mode = config_parser.get('global', 'mode')
+        empty_script = config_parser.get(mode, 'empty')
+        reduce_script = config_parser.get(mode, 'reduce')
+        return empty_script, reduce_script
     except ConfigParser.Error as error:
         raise ConfigFileError(str(error))
---
+++ scripts/areduce.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser
+import os
+import subprocess
+import sys
+
+from vsc.atools.int_ranges import int_ranges2set, InvalidRangeSpecError
+
+
+if __name__ == '__main__':
+    arg_parser = ArgumentParser(description='Reduce the output of multiple '
+                                            'tasks to a single file')
+    arg_parser.add_argument('-t', help='array ID range to consider')
+    arg_parser.add_argument('--pattern', help='file name pattern')
+    arg_parser.add_argument('--mode', choice=['text', 'csv'],
+                            default='text',
+                            help='predefined reduction mode to use')
+    script_group = arg_parser.add_argument_group(title='reduction scripts')
+    script_group.add_argument('--empty', help='script to create empty '
+                                              'output file to use')
+    script_group.add_argument('--reduce', help='script to reduce output')
+    arg_parser.add_argument('--out', required=True, help='output file name')
+    arg_parser.add_argument('--rm_orig', action='store_true',
+                            help='remove original output files')
+    arg_parser.add_argument('--quiet', action='store_true',
+                            help='do not warn on missing output files')
+    arg_parser.add_argument('--conf', help='configuration file')
+    options = arg_parser.parse_args()
+    if options.conf:
+        conf_filename = options.conf
+    else:
+        conf_filename = os.path.join(os.path.dirname(__file__),
+                                     '..', 'conf', 'atools.conf')
+    if ((not options.empty and options.reduce) or
+        (options.empty and not options.reduce)):
+        msg = ('### error: either both --empty and --reduce or neither '
+               'should be given\n')
+        sys.stderr.write(msg)
+        sys.exit(1)
+    try:
+        if options.empty:
+            empty_filename, reduce_filename = options.empty, options.reduce
+        else:
+            if options.mode:
+                mode = options.mode
+            else:
+                mode = get_default_mode(options.conf)
+            empty_script, reduce_script = get_mode_config(config_filename,
+                                                          mode)
+            empty_filename = os.join(os.path.dirname(__file__), '..',
+                                     'reduce', empty_script)
+            reduce_filename = os.join(os.path.dirname(__file__), '..',
+                                     'reduce', reduce_script)
+        array_ids = list(int_ranges2set(options.t))
+        if not array_ids:
+            sys.stderr.write('empty array ID set\n')
+            sys.exit(0)
+        array_ids.sort()
+        array_idx = 0
+        while True:
+            filename = options.pattern.format(t=array_ids[array_idx])
+            if os.path.exits(filename):
+                subprocess.check_call(empty_filename, options.out,
+                                      filename)
+                break
+            elif not options.quiet:
+                msg = "### warming: '{0}' does not exist\n".format(filename)
+                sys.stderr.write(msg)
+            array_id_idx += 1
+        for array_id_idx in xrange(array_id_idx, len(array_ids) + 1):
+            filename = options.pattern.format(t=array_ids[array_idx])
+            if os.path.exists(filename):
+                subprocess.check_call(reduce_filename, options.out,
+                                      filename)
+            elif not options.quiet:
+                msg = "### warming: '{0}' does not exist\n".format(filename)
+                sys.stderr.write(msg)
+    except subprocess.CalledProcessError as error:
+        msg = '### Subprocess error: {0}\n'.format(str(error))
+        sys.stderr.write(msg)
+        sys.exit(error.returncode)
+    except IOError as error:
+        msg = '### IOError: {0}'.format(str(error))
+        sys.stderr.write(msg)
+        sys.exit(error.errno)
+    except ConfigFileError as error:
+        msg = '### IOError: {0}'.format(str(error))
+        sys.stderr.write(msg)
+        sys.exit(error.errno)
