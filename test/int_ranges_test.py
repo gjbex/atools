eade9adbd12faa1e3864ca48786781346c97d424
Geert Jan Bex <geertjan.bex@uhasselt.be>
2016-09-12 07:54:58 +0200

    Added tests

--- test/int_ranges_test.py
+++ test/int_ranges_test.py
@@ -2,7 +2,7 @@
 
 import unittest
 
-from vsc.atools.int_ranges import int_ranges2set
+from vsc.atools.int_ranges import int_ranges2set, InvalidRangeSpecError
 
 
 class TestIntRanges(unittest.TestCase):
@@ -12,10 +12,39 @@ class TestIntRanges(unittest.TestCase):
         expected_set = set([2])
         self.assertEqual(expected_set, int_ranges2set('2'))
 
+    def test_ind2set(self):
+        expected_set = set([2, 4, 8])
+        self.assertEqual(expected_set, int_ranges2set('2,4,8'))
+
     def test_simple2set(self):
         expected_set = set(range(1, 11))
         self.assertEqual(expected_set, int_ranges2set('1-10'))
 
+    def test_multiple2set(self):
+        expected_set = set(range(1, 11)) | set(range(21, 31))
+        self.assertEqual(expected_set, int_ranges2set('1-10,21-30'))
 
+    def test_multiple_ind2set(self):
+        expected_set = set(range(5, 11)) | set(range(21, 31))
+        expected_set.add(3)
+        expected_set.add(15)
+        expected_set.add(35)
+        self.assertEqual(expected_set, int_ranges2set('3,5-10,15,35,21-30'))
+
+    def test_invalid_bounds2set(self):
+        try:
+            _ = int_ranges2set('5-1')
+            self.assertTrue(False)
+        except InvalidRangeSpecError:
+            pass
+
+    def test_invalid_expr2set(self):
+        try:
+            _ = int_ranges2set('-1')
+            self.assertTrue(False)
+        except InvalidRangeSpecError:
+            pass
+
+    
 if __name__ == '__main__':
     unittest.main()
