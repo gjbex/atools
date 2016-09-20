9cc638daadfc8ef64a58258352b32a10259a1710
Geert Jan Bex <geertjan.bex@uhasselt.be>
2016-09-20 09:01:26 +0200

    Code for creating shell variable definitions factored into odule

--- lib/aenv.py
+++ lib/aenv.py
@@ -5,24 +5,12 @@ import csv
 import os
 import sys
 
-from vsc.atools.config import get_var_config, ConfigFileError
+from vsc.shell import create_var_defs, get_shells, UnknownShellError
+from vsc.atools.config import (get_var_config, get_default_shell,
+                               ConfigFileError)
 from vsc.atools.utils import EnvVarError, EndOfFileError
 
 
-def create_bash_statements(row):
-    '''given a CSV row, create bash export statements'''
-    def_strs = []
-    for variable in row:
-        def_strs.append('export {0}="{1}"'.format(variable, row[variable]))
-    return os.linesep.join(def_strs)
-
-
-def create_csh_statements(row):
-    '''given a CSV row, create csh export statements'''
-    def_strs = []
-    for variable in row:
-        def_strs.append('setenv {0} "{1}"'.format(variable, row[variable]))
-    return os.linesep.join(def_strs)
 
 
 if __name__ == '__main__':
@@ -33,25 +21,22 @@ if __name__ == '__main__':
                             help='CSV files to use')
     arg_parser.add_argument('--id', type=int,
                             help='line to use from CSV file')
-    arg_parser.add_argument('--shell', default='bash',
-                            choices=['bash', 'tcsh', 'csh', 'sh'],
+    arg_parser.add_argument('--shell', choices=get_shells(),
                             help='shell to generate defintions for')
     arg_parser.add_argument('--sniff', type=int, default=1024,
                             help='number of bytes to sniff for CSV dialect')
     arg_parser.add_argument('--conf', help='configuration file')
     options = arg_parser.parse_args()
-    formatters = {
-        'bash': create_bash_statements,
-        'sh': create_bash_statements,
-        'tcsh': create_csh_statements,
-        'csh': create_csh_statements,
-    }
     if options.conf:
         conf_filename = options.conf
     else:
         conf_filename = os.path.join(os.path.dirname(__file__),
                                      '..', 'conf', 'atools.conf')
     try:
+        if options.shell:
+            shell = options.shell
+        else:
+            shell = get_default_shell(conf_filename)
         var_names = get_var_config(conf_filename)
         if options.id:
             work_item_id = options.id
@@ -71,7 +56,7 @@ if __name__ == '__main__':
                 for row in csv_reader:
                     row_nr += 1
                     if row_nr == work_item_id:
-                        print(formatters[options.shell](row))
+                        print(create_var_defs(row, shell))
                         break
                 else:
                     raise EndOfFileError(work_item_id, row_nr)
@@ -88,6 +73,10 @@ if __name__ == '__main__':
         sys.stderr.write(msg)
         sys.exit(error.errno)
     except ConfigFileError as error:
+        msg = '### error: {0}'.format(str(error))
+        sys.stderr.write(msg)
+        sys.exit(error.errno)
+    except UnknownShellError as error:
         msg = '### error: {0}'.format(str(error))
         sys.stderr.write(msg)
         sys.exit(error.errno)
---
+++ lib/vsc/shell.py
@@ -0,0 +1,58 @@
+'''Module with some useful shell related functions'''
+
+import os
+
+
+class UnknownShellError(Exception):
+    '''Exception denoting an unknow shell has been specified'''
+
+    def __init__(self, shell):
+        super(UnknownShellError, self).__init__()
+        self._shell = shell
+        self._errno = 2
+
+    def __str__(self):
+        return 'unknown shell: {0}'.format(self._shell)
+
+
+def create_bash_var_def(var, value):
+    '''given a name and a value, create bash export statement'''
+    return 'export {0}="{1}"'.format(var, value)
+
+
+def create_csh_var_def(var, value):
+    '''given a name and a value, create csh setenv statement'''
+    return 'setenv {0} "{1}"'.format(var, value)
+
+
+formatters = {
+    'bash': create_bash_var_def,
+    'sh': create_bash_var_def,
+    'tcsh': create_csh_var_def,
+    'csh': create_csh_var_def,
+}
+
+
+def get_shells():
+    return list(formatters.keys())
+
+
+def create_var_defs(row, shell):
+    '''given a dict-like object, row, create shell variable definition
+     statements'''
+    try:
+        formatter = formatters[shell]
+    except KeyError:
+        raise UnknownShellError(shell)
+    def_strs = []
+    for variable in row:
+        def_strs.append(formatter(variable, row[variable]))
+    return os.linesep.join(def_strs)
+
+
+def create_var_def(var, value, shell):
+    '''given a name and a value, create shell definition statement'''
+    try:
+        return formatters[shell](var, value)
+    except KeyError:
+        raise UnknownShellError(shell)
